382d79cf98aed11eaf284aec3947be1fb3066e2d
AeddAenye <flavymettis@gmail.com>
2024-01-31 20:25:21 +0700

    Разнёс локальные стили по файлам

--- test/src/components/CreateTask.vue
+++ test/src/components/CreateTask.vue
@@ -28,3 +28,40 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.inputarea {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 20px 50px;
+    padding: 20px;
+    background-color: var(--block-bg);
+    border-radius: 15px;
+}
+
+.inputarea .userinput {
+    width: 90%;
+    padding: 0px 20px;
+}
+
+.inputarea input {
+    width: 100%;
+    font-size: 1.5rem;
+    padding: 10px;
+    border-radius: 10px;
+    border: none;
+}
+
+.inputarea button.addtask  {
+    background-color: rgb(126, 187, 240);
+}
+
+.inputarea button:hover {
+    background-color: rgb(88, 170, 242);
+}
+
+.inputarea button:active {
+    scale: 0.95;
+}
+</style>
--- test/src/components/Task.vue
+++ test/src/components/Task.vue
@@ -20,9 +20,38 @@ export default {
     },
   },
   methods: {
-    deletetask(){
+    deletetask() {
       this.$emit("deltask")
     }
   }
 };
 </script>
+
+<style scoped>
+.task {
+  margin: 20px 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--block-bg);
+  border-radius: 15px;
+  padding: 20px;
+}
+
+.task .text {
+  width: calc(100% - 60px);
+  margin: 0px 20px;
+}
+
+button.delete {
+  background-color: rgb(245, 94, 94);
+}
+
+.task button.delete:hover {
+  background-color: rgb(240, 55, 55);
+}
+
+.task button.delete:active {
+  scale: 0.95;
+}
+</style>
